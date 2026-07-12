432c8281f483a251c82fbd23b39a856231cf21fe
wesobado-glitch <wesobado@gmail.com>
2026-07-12 17:00:25 +0300

    Update settings.py

--- recommendation_v1/settings.py
+++ recommendation_v1/settings.py
@@ -86,7 +86,7 @@ APPEND_SLASH = False
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
