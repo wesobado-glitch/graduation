5cf2d8be61e64ee4c343b17ce5fb66eb5f703187
wesobado-glitch <wesobado@gmail.com>
2026-07-12 17:03:15 +0300

    Update urls.py

--- analytics_bot/urls.py
+++ analytics_bot/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import chat
+from .views import chat,home
 
 
 urlpatterns = [
     path('analytics/', chat, name='chat'),
     path('analytics', chat, name='chat_no_slash'),
+    path('', home, name='ui'),
 ]
